a6c38f039cf0e4920a4ed6b8f222135b3852a18e
Evgeniy Roschin <evgeny689@yandex.ru>
2019-12-06 14:46:56 +0400

    correct

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,6 +1,7 @@
+/* eslint-disable linebreak-style */
 import findBy from '../app';
 
-test('Success found', () => {
+test('Success name, урон', () => {
   const finder = findBy('name', 'урон');
   const results = [
     { name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями' },
@@ -12,6 +13,23 @@ test('Success found', () => {
       name: 'урон',
       type: 'help',
       description: 'Страница описания элемента интерфейса',
+    },
+  ];
+  expect(results.filter(finder)).toEqual(example);
+});
+
+test('Success type, character', () => {
+  const finder = findBy('type', 'character');
+  const results = [
+    { name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями' },
+    { name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' },
+    { name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса' },
+  ];
+  const example = [
+    {
+      name: 'маг',
+      type: 'character',
+      description: 'Персонаж, обладающий магическими способностями',
     },
   ];
   expect(results.filter(finder)).toEqual(example);
--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,22 @@
-// // TODO: write your code here
-// import sum from './basic';
-
-// console.log('worked');
-
-// console.log(sum([1, 2]));
+/* eslint-disable func-names */
 export default function findBy(a, b) {
-    const field = a;
-    const value = b;
-    return function (element) {
-      if (element[field].includes(value)) return true;
-      return false;
-    };
-  }
+  const field = a;
+  const value = b;
+  return function (element) {
+    if (element[field].includes(value)) return true;
+    return false;
+  };
+}
+
+// такой вариант не проходит: includes не читается.
+
+// const findBy = (a, b) => {
+//   const field = a;
+//   const value = b;
+//   return (element) => {
+//     if (element[field].includes(value)) return true;
+//     return false;
+//   };
+// };
+
+// export default findBy();
