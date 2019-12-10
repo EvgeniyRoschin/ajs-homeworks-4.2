081e00959cbc168887027d3b98cec1876026b327
Evgeniy Roschin <evgeny689@yandex.ru>
2019-12-10 14:06:51 +0400

    corrects

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -34,3 +34,37 @@ test('Success type, character', () => {
   ];
   expect(results.filter(finder)).toEqual(example);
 });
+
+test('fail data', () => {
+  const finder = findBy('fefe', 'brbr');
+  const results = [
+    { name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями' },
+    { name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' },
+    { name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса' },
+  ];
+  const example = [];
+  expect(results.filter(finder)).toEqual(example);
+});
+
+test('duble result', () => {
+  const finder = findBy('type', 'attack');
+  const results = [
+    { name: 'маг', type: 'character', description: 'Персонаж, обладающий магическими способностями' },
+    { name: 'заклинание', type: 'attack', description: 'Атака магическим заклинанием' },
+    { name: 'зомби', type: 'attack', description: 'Атака' },
+    { name: 'урон', type: 'help', description: 'Страница описания элемента интерфейса' },
+  ];
+  const example = [
+    {
+      name: 'заклинание',
+      type: 'attack',
+      description: 'Атака магическим заклинанием',
+    },
+    {
+      name: 'зомби',
+      type: 'attack',
+      description: 'Атака',
+    },
+  ];
+  expect(results.filter(finder)).toEqual(example);
+});
--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ export default function findBy(a, b) {
   const field = a;
   const value = b;
   return function (element) {
-    if (element[field].includes(value)) return true;
+    if (element[field] !== undefined && element[field].includes(value)) return true;
     return false;
   };
 }
